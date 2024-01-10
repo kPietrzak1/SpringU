827091c57da18f12436b2ba8ee18c41d49dba7b3
kPietrzak <kpietrzak555@gmail.com>
2024-01-10 13:46:42 +0000

    first commit

---
+++ src/main/java/com/example/springu/SpringUApplication.java
@@ -0,0 +1,13 @@
+package com.example.springu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springu/SpringUApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
